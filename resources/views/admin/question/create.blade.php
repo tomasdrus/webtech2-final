23a2105c0043be21679e93162d62b4e3e61559a3
Tomáš Drus <tomasdrus@gmail.com>
2021-04-30 10:15:34 +0200

    Dynamically adding  selecting question with js

--- resources/views/admin/question/create.blade.php
+++ resources/views/admin/question/create.blade.php
@@ -35,7 +35,7 @@
         </label>
 
         {{-- type classical --}}
-        <section id="classical" class="type__sections">
+        <section id="classical" class="type__sections hidden">
             <label class="admin__label">Correct answer
                 <span class="text-red-500 ml-1">@error('testAnswer') {{$message}} @enderror</span>
                 <input type="text" name="testAnswer" value="{{ old('testAnswer') }}" class="admin__input">
@@ -43,22 +43,29 @@
         </section>
 
         {{-- type selecting --}}
-        <section id="selecting" class="type__sections hidden">
-            <div class="flex mb-5">
+{{--         <section id="selecting" class="type__sections">
+            <div class="flex items-end mb-5">
                 <label class="admin__label flex-1">Answer option 1
                     <span class="text-red-500 ml-1">@error('option[]') {{$message}} @enderror</span>
                     <input type="text" name="option[]" value="{{ old('option[]') }}" class="admin__input mb-0">
                 </label>
-                <input type="checkbox" name="rightness1" class="block cursor-pointer w-5 ml-2 mt-7 border border-gray-500">
+                <input type="checkbox" name="rightness1" class="block cursor-pointer w-5 h-5 mb-2 ml-2 border border-gray-500">
+                <button class="pr-0 py-1.5 pl-3 text-lg leading-6 text-red-500 hover:text-red-600  cursor-pointer"><i class="far fa-trash-alt"></i></button>
             </div>
+        </section> --}}
 
-            <div class="flex mb-5">
-                <label class="admin__label flex-1">Answer option 2
-                    <span class="text-red-500 ml-1">@error('option[]') {{$message}} @enderror</span>
-                    <input type="text" name="option[]" value="{{ old('option[]') }}" class="admin__input mb-0">
-                </label>
-                <input type="checkbox" name="rightness2" class="block cursor-pointer w-5 ml-2 mt-7 border border-gray-500">
-            </div>
+        <section id="selecting" class="type__sections">
+            <label class="admin__label mb-5 selecting__option">
+                <span>Answer option 1</span>
+                <span class="text-red-500 ml-1">@error('option[]') {{$message}} @enderror</span>
+                <div class="flex items-center gap-3 mt-2">
+                    <input type="text" name="option[]" value="{{ old('option[]') }}" class="admin__input my-0">
+                    <input type="checkbox" name="rightness1" class="block cursor-pointer w-5 h-5 border border-gray-500">
+                    <button type="button" class="option__delete text-xl text-red-500 hover:text-red-600 cursor-pointer outline-none"><i class="far fa-trash-alt"></i></button>
+                </div>
+            </label>
+
+            <button type="button" id="optionAdd" class="w-full rounded-md py-1.5 transition duration-100 text-sm text-green-600 border border-green-600 hover:bg-green-600 hover:text-white mb-5">Add new option field</button>
         </section>
 
         {{-- type pairing --}}
@@ -97,6 +104,7 @@
 </div>
 
 <script>
+    /* Toggle different question type showing */
     const typeSelect = document.getElementById('type');
     const typeSections = document.querySelectorAll('.type__sections');
 
@@ -109,6 +117,46 @@
             section.classList.add('hidden');
         });
     })
+
+    /* Option add new field */
+    const optionAddButton = document.getElementById('optionAdd');
+    const selectingContainer = document.getElementById('selecting');
+
+    optionAddButton.addEventListener('click', () => {
+        let elementCopy = optionAddButton.previousElementSibling.cloneNode(true);
+        selectingContainer.insertBefore(elementCopy, optionAddButton);
+
+        changeIndexing();
+        updateOptionDeleteListeners();
+    })
+
+    /* Option numbering */
+    const changeIndexing = () => {
+        const options = document.querySelectorAll('.selecting__option');
+        options.forEach((option, index) => {
+            option.querySelector('span').innerHTML = `Answer option ${index + 1}`;
+            option.querySelector('input[type="checkbox"]').setAttribute('name', `rightness${index + 1}`)
+        })
+    }
+
+    /* Option delete field */
+    const updateOptionDeleteListeners = () => {
+        const optionDeleteButtons = document.querySelectorAll('.option__delete');
+        const minOptions = 1;
+
+        optionDeleteButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                if(document.querySelectorAll('.option__delete').length <= minOptions){
+                    return;
+                }
+                button.parentElement.parentElement.remove();
+                changeIndexing();
+            })
+        });
+    }
+
+    updateOptionDeleteListeners();
+    
 </script>
 
 @endsection
