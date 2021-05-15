974233c186c83e87c0b7830cd57a3e080fee9568
fiilo <samuhelfilip@gmail.com>
2021-05-15 18:19:21 +0200

    add docker

---
+++ public/js/countdown.js
@@ -0,0 +1,29 @@
+const token = document.querySelector('div[id=examTime]').textContent
+
+console.log(token);
+
+    var d1 = new Date ()   
+    console.log(d1);
+    d1.setSeconds ( d1.getSeconds() + parseInt(token) );
+    console.log(d1);
+    
+    var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = d1 - now;
+        
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+    
+    document.getElementById("displayTime").innerText =  hours + "h "
+    + minutes + "m " + seconds + "s ";
+        
+
+    if (distance < 1) {
+        clearInterval(x);
+        document.getElementById("examForm").submit();
+    }
+    }, 1000);
--- resources/views/admin/info/documentation.blade.php
+++ resources/views/admin/info/documentation.blade.php
@@ -3,26 +3,41 @@
 @section('content')
 
 <div class="bg-white p-5 m-5 rounded-lg">
-    <h4 class="font-semibold mb-3">Tasks list</h4>
+    <h4 class="font-semibold mb-3">Documentation</h4>
 
     <table class="w-full bg-white border border-gray-200">
         <thead class="bg-gray-200 text-gray-700 text-sm uppercase font-semibold text-left">
             <tr>
-                <th class="py-3 px-4 font-semibold text-sm w-5">Package</th>
-                <th class="py-3 px-4 font-semibold text-sm text-center">aaa</th>
-                <th class="py-3 px-4 font-semibold text-sm text-center">bb</th>
-                <th class="py-3 px-4 font-semibold text-sm text-center">cc</th>
-                <th class="py-3 px-4 font-semibold text-sm text-center">dd</th>
+                <th class="py-3 px-4 font-semibold text-sm w-5">časti </th>
+                <th class="py-3 px-4 font-semibold text-sm text-center">Package</th>    
                 </th>
             </tr>
         </thead>
         <tbody class="text-gray-600">
             <tr>
-                <td class="py-3 px-4 border border-gray-200">xxx</td>
-                <td class="py-3 px-4 border border-gray-200 text-center">xx</td>
-                <td class="py-3 px-4 border border-gray-200 text-center">xx</td>
-                <td class="py-3 px-4 border border-gray-200 text-center">xx</td>
-                <td class="py-3 px-4 border border-gray-200 text-center">xx</td>
+                <td class="py-3 px-4 border border-gray-200">matematicka</td>
+                <td class="py-3 px-4 border border-gray-200 text-center">https://mathlive.io/</td>  
+            </tr>
+            <tr>
+                <td class="py-3 px-4 border border-gray-200">pdf</td>
+                <td class="py-3 px-4 border border-gray-200 text-center"> https://github.com/barryvdh/laravel-dompdf</td>  
+            </tr>
+            <tr>
+                <td class="py-3 px-4 border border-gray-200">csv</td>
+                <td class="py-3 px-4 border border-gray-200 text-center">Použili sme: fputcsv</td>  
+            </tr>
+            <tr>
+                <td class="py-3 px-4 border border-gray-200">obrázok</td>
+                <td class="py-3 px-4 border border-gray-200 text-center">javascript</td>
+            </tr>
+            <tr>
+                <td class="py-3 px-4 border border-gray-200">Ikony</td>
+                <td class="py-3 px-4 border border-gray-200 text-center">https://fontawesome.com/v4.7.0/icons/</td>
+            </tr>
+            <tr>
+                <td class="py-3 px-4 border border-gray-200">CSS</td>
+                <td class="py-3 px-4 border border-gray-200 text-center">https://tailwindcss.com/</td>
+            
             </tr>
         </tbody>
     </table>
