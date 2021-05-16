4f4b8cab635f9a36792bc7a6aeb55d2eb31eebbe
Samuel Dubovec <samodubovec@gmail.com>
2021-05-16 13:05:17 +0200

    Changed doc language to eng

--- resources/views/admin/info/documentation.blade.php
+++ resources/views/admin/info/documentation.blade.php
@@ -8,36 +8,36 @@
     <table class="w-full bg-white border border-gray-200">
         <thead class="bg-gray-200 text-gray-700 text-sm uppercase font-semibold text-left">
             <tr>
-                <th class="py-3 px-4 font-semibold text-sm w-5">časti </th>
-                <th class="py-3 px-4 font-semibold text-sm text-center">Package</th>    
+                <th class="py-3 px-4 font-semibold text-sm w-5">parts </th>
+                <th class="py-3 px-4 font-semibold text-sm text-center">Package</th>
                 </th>
             </tr>
         </thead>
         <tbody class="text-gray-600">
             <tr>
-                <td class="py-3 px-4 border border-gray-200">matematicka</td>
-                <td class="py-3 px-4 border border-gray-200 text-center">https://mathlive.io/</td>  
+                <td class="py-3 px-4 border border-gray-200">math</td>
+                <td class="py-3 px-4 border border-gray-200 text-center">https://mathlive.io/</td>
             </tr>
             <tr>
                 <td class="py-3 px-4 border border-gray-200">pdf</td>
-                <td class="py-3 px-4 border border-gray-200 text-center"> https://github.com/barryvdh/laravel-dompdf</td>  
+                <td class="py-3 px-4 border border-gray-200 text-center"> https://github.com/barryvdh/laravel-dompdf</td>
             </tr>
             <tr>
                 <td class="py-3 px-4 border border-gray-200">csv</td>
-                <td class="py-3 px-4 border border-gray-200 text-center">Použili sme: fputcsv</td>  
+                <td class="py-3 px-4 border border-gray-200 text-center">Použili sme: fputcsv</td>
             </tr>
             <tr>
-                <td class="py-3 px-4 border border-gray-200">obrázok</td>
+                <td class="py-3 px-4 border border-gray-200">image</td>
                 <td class="py-3 px-4 border border-gray-200 text-center">javascript</td>
             </tr>
             <tr>
-                <td class="py-3 px-4 border border-gray-200">Ikony</td>
+                <td class="py-3 px-4 border border-gray-200">icons</td>
                 <td class="py-3 px-4 border border-gray-200 text-center">https://fontawesome.com/v4.7.0/icons/</td>
             </tr>
             <tr>
                 <td class="py-3 px-4 border border-gray-200">CSS</td>
                 <td class="py-3 px-4 border border-gray-200 text-center">https://tailwindcss.com/</td>
-            
+
             </tr>
         </tbody>
     </table>
